5f896e3b763078e426733c972da55b5994824f66
MR-aditya-45 <MR-aditya-45@users.noreply.github.com>
2026-04-09 20:02:19 +0530

    fix render port issue

--- app.py
+++ app.py
@@ -439,5 +439,8 @@ def download_plan(student_id):
     )
 
 
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
